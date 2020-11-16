02a3288c01515e7bdaab4710c60feeae959ad821
浜田真帆 <hamadamaho@hamadamahonoMacBook.local>
2020-11-17 00:01:13 +0900

    create component

---
+++ react-sample/src/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Article from './Article.jsx'
+
+
+class Test extends React.Component {
+	return() {
+		<div>
+			<Article />
+		</div>
+	}
+}
+
+export default Test;
